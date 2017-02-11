1dea20a1e6b93a6b34f886f8bb321bf9b2652fe2
Jason Marshall <jasonnz@gmail.com>
2017-02-11 17:12:33 +1300

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,56 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/Todo');
+
+
+beforeEach(() => {
+    Todo.remove({}).then(()=> done);
+})
+
+describe('POST todos', () => {
+    // 1st test case
+    it('Should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1); 
+                expect(todos[0].text).toBe(text); 
+                done();                                        
+            }).catch((err) => done(err));
+        });
+    });
+
+    // 2nd test case
+    it('Should not create a todo with invalise todo data', (done) => {
+
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0); 
+                done();                                        
+            }).catch((err) => done(err));
+        });
+    });
+
+});
