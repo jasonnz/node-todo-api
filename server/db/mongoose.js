cfa1cfee3b5ab63c584a435939f6dd4b2ccfc560
Jason Marshall <jasonnz@gmail.com>
2017-02-12 21:37:32 +1300

    added GET /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,3 +4,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+//npm uninstall mongoose@4.8.1 --save 
+//npm install mongoose@4.8.1 --save 
+//npm install mongoose@4.7.2 --save 
+//npm uninstall mongoose@4.7.2 --save 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectId} = require('mongodb');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
 
@@ -31,6 +32,30 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
         console.log(err)
     });
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+    var id = req.params.id;
+    
+    if (!ObjectId.isValid(id)) {
+        console.log('id not valid: ');
+        return res.status(404).send();
+    } 
+
+    Todo.findById({_id: id}).then((todo) => {
+        if (!todo) {
+            console.log('No todo _id found!');
+            return res.status(404).send();
+        }
+        console.log('todo by _id: ', JSON.stringify(todo));
+        res.status(200).send(JSON.stringify(todo));
+    }).catch((err) => {
+        console.log('Processing error has occured!');
+        res.status(400).send(err)
+    });
+
 });
 
 app.listen(3000, ()=> {
